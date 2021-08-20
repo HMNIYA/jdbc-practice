5895f6ff03ad8326083c8b9a51af719801352c86
YegorAlexandrov <farlyteem@gmail.com>
2021-08-20 20:26:13 +0600

    Экспорт данных таблица в файл .csv

--- src/main/java/com/example/jdbcpractice/Employee.java
+++ src/main/java/com/example/jdbcpractice/Employee.java
@@ -1,7 +1,49 @@
 package com.example.jdbcpractice;
 
-public class Employee {
+import java.io.Serializable;
 
-    public Employee() {
+public class Employee implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private String id;
+        private String firstName;
+        private String lastName;
+        private int managerId;
+        private int departmentId;
+        private int salary;
+    public String getId()
+        {
+            return id;
+        }
+        public String getFirstName()
+        {
+            return firstName;
+        }
+        public String getLastName()
+        {
+            return lastName;
+        }
+        public int getManagerId()
+        {
+            return managerId;
+        }
+        public int getDepartmentId()
+        {
+            return departmentId;
+        }
+        public int getSalary(String age)
+        {
+            return salary;
+        }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", managerId=" + managerId +
+                ", departmentId=" + departmentId +
+                ", salary=" + salary +
+                '}';
     }
 }
--- src/main/java/com/example/jdbcpractice/JDBCExecutor.java
+++ src/main/java/com/example/jdbcpractice/JDBCExecutor.java
@@ -1,14 +1,59 @@
 package com.example.jdbcpractice;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+
+import static java.lang.System.exit;
 
 public class JDBCExecutor {
     public void saveEmployees(List<Employee> employeeList) {
+        String URL = "jdbc:h2:mem:test";
+        String USER = "sa";
+        String PASSWORD = "";
 
+        try (Connection conn = DriverManager
+                .getConnection(URL, USER, PASSWORD);) {
+            Statement statement = conn.createStatement();
+            //ResultSet res = statement.executeQuery("select * from EMPLOYEE");
+            //FileWriter fw = new FileWriter("file_for_project.csv");
+            if(conn.isClosed()) statement = conn.createStatement();
+            String sql = "select * from EMPLOYEE";
+
+            ResultSet result = statement.executeQuery(sql);
+
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter("file_for_project.csv"));
+
+            // write header line containing column names
+            fileWriter.write("first_name,last_name,manager_id,department_id,salary");
+
+            while (result.next()) {
+                int id = result.getInt("id");
+                String firstName = result.getString("first_name");
+                String lastName = result.getString("last_name");
+                int managerId = result.getInt("manager_id");
+                int departmentId = result.getInt("department_id");
+                int salary = result.getInt("salary");
+
+                String line = String.format("%d,%s,%s,%d,%d,%d",id, firstName, lastName, managerId, departmentId, salary);
+
+                fileWriter.newLine();
+                fileWriter.write(line);
+            }
+
+            statement.close();
+            fileWriter.close();
+        } catch (SQLException | IOException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public List<Employee> findEmployees() {
-        return Collections.emptyList();
+        public List<Employee> findEmployees() {
+            return Collections.emptyList();
     }
 }
